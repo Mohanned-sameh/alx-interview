30e539c270dd935b94eec5a493fbd377573b271e
Mohanned-sameh <mohanned2678@gmail.com>
2024-06-03 19:47:08 +0300

    Refactor Star Wars character retrieval logic for better readability and maintainability

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,16 +1,27 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/';
-const id = process.argv[2];
 
-request(url, (error, response, body) => {
-  for (const film of JSON.parse(body).results) {
-    if (film.episode_id === parseInt(id)) {
-      for (const character of film.characters) {
-        request(character, (error, response, body) => {
-          console.log(JSON.parse(body).name);
-        });
-      }
+const request = require('request');
+
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
+    }
+  });
+};
+
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
     }
   }
-});
+);
